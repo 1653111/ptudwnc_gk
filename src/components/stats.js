95b2c675547274571627295e57417b059127005d
1653111 <1653111@student.hcmus.edu.vn>
2020-05-06 22:01:19 +0700

    commit final

--- src/components/stats.js
+++ src/components/stats.js
@@ -5,31 +5,30 @@ import ChartVN from './StatsVN';
 import ChartTotal from './StatsTotal'
 
 import {Container, Card} from 'react-bootstrap';
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const StaticDashboard = (props) => {
     return ( <Container>
          <h4>Line Chart</h4>
-        <Row>
-            <Col xs={6}>
-            <Card>
-                <Card.Title>Việt Nam</Card.Title>
-                 <Card.Body>
-                     <ChartVN></ChartVN>
-                 </Card.Body> 
+        <Col>
+            <Col xs={10}>
+                <Card>
+                    <Card.Title>Việt Nam</Card.Title>
+                    <Card.Body>
+                        <ChartVN></ChartVN>
+                    </Card.Body> 
                 </Card> 
             </Col>
-            <Col xs={6}>
-            <Card>
-                <Card.Title>Global</Card.Title>
-                 <Card.Body>
-                    <ChartTotal></ChartTotal>
-                 </Card.Body> 
+            <Col xs={10}>
+                <Card>
+                    <Card.Title>Global</Card.Title>
+                    <Card.Body>
+                        <ChartTotal></ChartTotal>
+                    </Card.Body> 
                 </Card> 
             </Col>   
-        </Row>
+        </Col>
     </Container> 
     );
 };
