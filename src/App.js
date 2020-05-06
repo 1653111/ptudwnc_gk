0d4d96f79326bed28e4801d081cdc8c091fed06b
1653111 <1653111@student.hcmus.edu.vn>
2020-05-06 19:06:34 +0700

    commit map again

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
-
-import logo from './logo.svg';
 import './App.css';
-import CovidGoogleMap from "./components/CovidGoogleMap";
-import PatientInfo from "./components/PatientInfo";
-import CovidDashboard from "./components/CovidDashboard";
+import CovidDashboard from './components/CovidDashboard';
 
 function App() {
     return (
-        <CovidDashboard/>
+        <CovidDashboard />
     );
 }
 
